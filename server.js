67d1c3958f047747e351bc66890b675f25ac15e6
AKSHARAS <as688829@gmail.com>
2017-08-04 15:51:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one',function (req,res){
+app.get('/article-one',function (req,res){
     res.send('request of article one is requested and responded my browser here...');
 });
 
-app.get('/article two',function (req,res){
+app.get('/article-two',function (req,res){
     res.send('hey  i am article 2');
 });
 
-app.get('/article three',function (req,res){
+app.get('/article-three',function (req,res){
     res.send('hey its me article 3...');
 });
 
